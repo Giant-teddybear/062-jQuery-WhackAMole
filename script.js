dc8d84b4eceeb1d5fe84d308bcacdb6366fdbec3
Giant-teddybear <77467622+Giant-teddybear@users.noreply.github.com>
2021-11-19 05:46:54 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,21 @@
+$(".whack1").click(function(){
+    $(".mole1").hide(".Whack Me");
+});
+    
+$(".whack2").click(function(){
+    $(".mole-two").hide();
+});
+    
+$(".whack3").click(function(){
+    $(".third-mole").hide();
+});
+    
+$(".reset").click(function(){
+    $(".mole1").show();
+    $(".mole-two").show();
+        $(".third-mole").show();
+});
+    
+
+  
+    
